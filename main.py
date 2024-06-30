f44b4e3fd33311dbce143b194fe1877ca370b11a
Gonzalo <gmartipo@emeal.nttdata.com>
2024-06-30 11:43:34 +0200

    Final step

--- main.py
+++ main.py
@@ -10,24 +10,38 @@ def substitute(array: list, regex: str) -> list:
     return new_array
 
 
-def print_only_explicit(array: list, instruction: str) -> None:
+def print_only_explicit(array: list, instruction: str) -> str:
+    cad = ""
     if instruction[-1] == "p":
         instruction = instruction.rstrip("p")
         first, last = instruction.split(",")
         for i in range(int(first)-1, int(last)):
+            cad += array[i]
             print(array[i].rstrip())
+    return cad
 
 
-def filtering(array: list, word: str) -> None:
+def filtering(array: list, word: str) -> str:
+    cad = ""
     for quote in array:
         if word in quote:
+            cad += word
             print(quote.rstrip())
+    return cad
 
 
-def print_with_double_line_jump(array: list) -> None:
+def print_with_double_line_jump(array: list) -> str:
+    cad = ""
     for element in array:
         if element != "\n":
             print(element)
+            cad += element
+    return cad
+
+
+def overwrite_file(info: str, filename: str) -> None:
+    with open(filename,"w") as file:
+        file.write(info)
 
 
 def main() -> None:
@@ -50,28 +64,38 @@ def main() -> None:
                         help='Does not output the result of the command '
                         'unless there is explicit instruction to'
                         )
-
+    parser.add_argument('-i',
+                        '--overwrite',
+                        action='store_true',
+                        help='overwrittes the original file '
+                        )
     args = parser.parse_args()
     with open(args.filename, "r") as read_file:
         content = read_file.readlines()
     parts_of_regex = args.regex.split("/")
+    output = ""
     if parts_of_regex[0] == "s" and len(parts_of_regex) == 4:
         changed_quotes = substitute(array=content, regex=args.regex)
         if not args.quiet:
             for qu in changed_quotes:
+                output += qu
                 print(qu.rstrip())
         else:
             _, _, new_word, _ = parts_of_regex
             for quo in changed_quotes:
                 if new_word in quo:
+                    output += quo
                     print(quo.rstrip())
+
     elif len(parts_of_regex) == 1 and parts_of_regex[0] == "G":
-        print_with_double_line_jump(array=content)
+        output = print_with_double_line_jump(array=content)
     elif len(parts_of_regex) == 3:
-        filtering(array=content, word=parts_of_regex[1])
+        output = filtering(array=content, word=parts_of_regex[1])
 
     elif len(parts_of_regex) == 1:
-        print_only_explicit(array=content, instruction=args.regex)
+        output = print_only_explicit(array=content, instruction=args.regex)
+    if args.overwrite:
+        overwrite_file(info=output, filename=args.filename)
 
 
 if __name__ == '__main__':
